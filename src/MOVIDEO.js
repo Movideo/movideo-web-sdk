cb89ddb2cde41e45baa6831cb029e5a5ca291bc4
Milos Dakic <hello@milosdakic.com>
2015-04-15 15:25:04 +1000

    Initial feature buildup.

--- src/MOVIDEO.js
+++ src/MOVIDEO.js
@@ -1,4 +1,69 @@
 'use strict'
 
-MOVIDEO.VERSION = require('../package.json').verion;
+Movideo.DEFAULT_HOST      = 'api.movideo.com';
+Movideo.DEFAULT_PORT      = '80';
+Movideo.DEFAULT_BASE_PATH = '/rest/';
+Movideo.DEFAULT_FORMAT    = 'json';
 
+Movideo.DEFAULT_TIMEOUT   = require('http').createServer().timeout;
+
+Movideo.PACKAGE_VERSION   = require('../package.json').verion;
+
+var resources = {
+  Session : require('./resources/Session'),
+  // Application: require('./resources/Application'),
+  // Playlist: require('./resources/Playlist'),
+  // Media: require('./resources/Media'),
+  // Tag: require('./resources/Tag'),
+  // Geolocation: require('./resources/Geolocation')
+};
+
+Movideo.resources = resources;
+
+function Movideo(key, alias) {
+
+  if (!this instanceof Movideo) {
+    return new Movideo(key, alias);
+  }
+
+  this._api = {
+    auth     : null,
+    alias    : null,
+    host     : Movideo.DEFAULT_HOST,
+    port     : Movideo.DEFAULT_PORT,
+    timeout  : Movideo.DEFAULT_TIMEOUT,
+  };
+
+  this._prepResources();
+  this.setApiKey(key);
+  this.setApiAlias(alias);
+}
+
+Movideo.prototype = {
+
+  setApiAlias: function(alias) {
+    this._setApiField('alias', alias);
+  },
+
+  _setApiField: function(key, value) {
+    this._api[key] = value;
+  },
+
+  getApiField: function(key) {
+    return this._api[key];
+  },
+
+  getConstant: function(c) {
+    return Movideo[c];
+  },
+
+  _prepResources: function() {
+    for (var name in resources) {
+      this[
+        name[0].toLowerCase() + name.substring(1)
+      ] = new resources[name](this);
+    }
+  }
+}
+
+module.exports = Movideo;
